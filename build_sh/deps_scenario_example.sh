7cf6c47439786c90e8d5d7296ecb83374d410716
skalexey <skorohodov-ad@narod.ru>
2022-06-06 12:34:51 +0300

    Wrapped deps_scenario into a function

--- build_sh/deps_scenario_example.sh
+++ build_sh/deps_scenario_example.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-source dependencies.sh
-source deps_config.sh
+function deps_scenario()
+{
+    source dependencies.sh
+    source deps_config.sh
 
-download_dependency "$vlDirRel" "$depsLocationFull" "git@github.com:skalexey/VL.git"
-download_dependency "$rapidjsonDirRel" "$depsLocationFull" "https://github.com/Tencent/rapidjson.git"
+    download_dependency "$vlDirRel" "$depsLocationFull" "git@github.com:skalexey/VL.git"
+    download_dependency "$rapidjsonDirRel" "$depsLocationFull" "https://github.com/Tencent/rapidjson.git"
+}
 
+deps_scenario $@
