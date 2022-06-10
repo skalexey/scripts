63cfd3eef2ed4d1d9ff06c772b6b9ce6ffc26037
Alex <skorohodov-ad@narod.ru>
2022-06-10 17:23:32 +0300

    Branch template_path for different os'es

--- automation/templates/templates_config.sh
+++ automation/templates/templates_config.sh
@@ -2,4 +2,11 @@
 
 # this config is copied to environment directories,
 # so it should contain absolute paths not dependent on its location
-templates_dir="${HOME}/Projects/Templates"
+# or paths to scripts from the environment
+
+source os.sh
+if is_mac ; then
+	templates_dir="${HOME}/Projects/templates"
+else
+	templates_dir="${HOME}/Projects/Templates"
+fi
