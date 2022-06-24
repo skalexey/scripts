136036598d7cf7913fd7a907c5b203dd0b08faa8
skalexey <skorohodov-ad@narod.ru>
2022-06-24 14:05:25 +0300

    job_template_job update: Add missed includes, _job suffix by default

--- automation/templates/job_template_job.sh
+++ automation/templates/job_template_job.sh
@@ -6,13 +6,17 @@
 function job_template_job()
 {
 	source automation_config.sh
+	includes=(
+		"$scripts_dir/include/os.sh" \
+	)
+	env_include ${includes[@]}
 	source templates_config.sh
 	source create_from_template_job.sh
 	source log.sh
 	local log_prefix="[job_template_job]: "
 	[ -z "$1" ] && log_error "No job name provided" && return 1 || local job_name="$1"
 	[ -z "$2" ] && log_error "No target path provided" && return 2 || local target_path="$2"
-	create_from_template_job "$templates_dir/Scripts/job_template.sh" "$job_name" "$target_path"
+	create_from_template_job "$templates_dir/Scripts/job_template.sh" "${job_name}_job" "$target_path"
 	return $?
 }
 
