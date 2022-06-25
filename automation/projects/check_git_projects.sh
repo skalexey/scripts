2896539abe0075d3c52a76cd29debfa3451a457f
skalexey <skorohodov-ad@narod.ru>
2022-06-25 14:22:55 +0300

    Update git check scripts: support filtered list in status check script. Some improvements

--- automation/projects/check_git_projects.sh
+++ automation/projects/check_git_projects.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-#$THIS_DIR/../run.sh $THIS_DIR/all_projects_job.sh $THIS_DIR/../include/print_args_job.sh
-$THIS_DIR/../run.sh $THIS_DIR/all_projects_job.sh $THIS_DIR/../include/helpers/git_check_job.sh $THIS_DIR/../include/helpers/git_ask_and_commit_job.sh ${@:1}
+source $THIS_DIR/../automation_config.sh
+
+filtered_fname="$automation_dir/projects/tmp/filtered_projects_to_check.txt"
+echo "filtered_fname: $filtered_fname"
+if [ ! -f "$filtered_fname" ]; then
+	$THIS_DIR/../run.sh $THIS_DIR/all_projects_job.sh $THIS_DIR/../include/user_filter_job.sh "$filtered_fname"
+fi
+
+$THIS_DIR/../run.sh \
+	$THIS_DIR/../include/file_list_job.sh \
+		"$THIS_DIR/tmp/filtered_projects_to_check.txt" \
+		$THIS_DIR/../include/helpers/git_check_job.sh \
+			$THIS_DIR/../include/helpers/git_ask_and_commit_job.sh ${@:1}
 
