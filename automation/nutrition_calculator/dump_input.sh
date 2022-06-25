d532435de4718bdeb14f9ebcfe1d37117055f79c
skalexey <skorohodov-ad@narod.ru>
2022-06-25 13:53:42 +0300

    Use new resources directory in nutrition_calculator/dump_input.sh

--- automation/nutrition_calculator/dump_input.sh
+++ automation/nutrition_calculator/dump_input.sh
@@ -2,12 +2,12 @@
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CUR_DIR=${PWD}
 proj_dir=~/Projects/C++/nutrition_calculator
-bin_dir=$proj_dir/Build-cmake
+res_dir=${HOME}/AppData/Local/Temp
 dumps_dir=$proj_dir/tmp/input_dump
 echo "dumps_dir: '$dumps_dir'"
 [ ! -d "$dumps_dir" ] && mkdir -p $dumps_dir
 t=`date +%F-%T`
 set -x #echo on
-cp $bin_dir/input.txt $dumps_dir/input-$t.txt
-cp $bin_dir/item_info.txt $dumps_dir/item_info-$t.txt
+cp $res_dir/input.txt $dumps_dir/input-$t.txt
+cp $res_dir/item_info.txt $dumps_dir/item_info-$t.txt
 #set -x #echo off
