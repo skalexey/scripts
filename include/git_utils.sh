f4d48f985b6c0412069ce9ede469dd2fe46f0e28
skalexey <skorohodov-ad@narod.ru>
2022-06-27 14:28:41 +0300

    git_utils update: added git_check_stash, fixes in git_pull

--- include/git_utils.sh
+++ include/git_utils.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+function git_check_stash()
+{
+	source log.sh
+	local log_prefix="[git_check_stash]: "
+
+	# arguments
+	if [ ! -z $1 ]; then # directory path. If not provided then this directory is used
+		local path=$1
+		local cur_dir=${PWD}
+		cd "$path"
+	fi
+	res=false
+	local stash_marker="git_utils.sh script stash"
+	local stash_list_res=$(git stash list | grep "$stash_marker")
+	if [ ! -z "$stash_list_res" ]; then
+		res=true
+	fi
+	
+	[ ! -z cur_dir ] && cd "$cur_dir"
+	
+	$res
+}
+
 function git_pull()
 {
 	source log.sh
-	local log_prefix="\033[0;32m[git_pull]: "
-	local log_postfix="\033[0m"
+	local log_prefix="[git_pull]: "
 
 	# arguments
 	if [ ! -z $1 ]; then # directory path. If not provided then this directory is used
@@ -15,20 +37,20 @@ function git_pull()
 
 	local status_res=$(git status | grep "Changes not staged for commit")
 	local stash_marker="git_utils.sh script stash"
-	if [ ! -z $status_res ]; then
-		log "Stash local work"
-		git stash -m $stash_marker
+	if [ ! -z "$status_res" ]; then
+		log_info "Stash the local work"
+		git stash -m "$stash_marker"
+		[ $? -eq 0 ] && log "Stashed" || (log_error "Error while stashing. Stop the job" && return 1)
 	fi
 	local branch=$(git rev-parse --abbrev-ref HEAD)
 	log "Current branch: '$branch'"
 	log "Pull command: git pull origin ${branch} --rebase"
 	git pull origin ${branch} --rebase
 
-	if [ ! -z $status_res ]
-	then
-		log "Pop back the local work"
-		stash_list_res=$(git stash list | grep $stash_marker)
-		if [ ! -z $stash_list_res ]; then
+	if [ ! -z "$status_res" ]; then
+		log_info "Pop back the local work"
+		local stash_list_res=$(git stash list | grep "$stash_marker")
+		if [ ! -z "$stash_list_res" ]; then
 			git stash pop
 		fi
 	fi
