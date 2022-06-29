8d97f5cefb5ac39a1a74b8628316bc8c3d745432
skalexey <skorohodov-ad@narod.ru>
2022-06-29 13:56:52 +0300

    Add projects/deliver_file.sh automation script

---
+++ automation/projects/deliver_file.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function deliver_file()
+{
+	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+	source $THIS_DIR/../automation_config.sh
+	source $THIS_DIR/../../include/log.sh
+	local log_prefix="[deliver_file]: "
+	[ -z "$1" ] && log_error "No file path provided" && return 1 || local fpath="$1"
+	[ ! -f "$fpath" ] && log_error "Not existent file provided: '$fpath'" && return 2
+
+	source $THIS_DIR/../../include/file_utils.sh
+	local ffullpath=$(full_path "$fpath")
+
+	$THIS_DIR/../run.sh \
+		$THIS_DIR/all_projects_job.sh \
+			$THIS_DIR/../include/swap_args_job.sh \
+			"$ffullpath" \
+			$THIS_DIR/../include/copy_job.sh
+	return 0
+}
+
+deliver_file $@
+[ $? -ne 0 ] && exit
