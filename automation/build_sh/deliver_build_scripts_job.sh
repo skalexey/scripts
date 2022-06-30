154cbe2b1c1ae6266476b2a3a24b55ff12ee4217
skalexey <skorohodov-ad@narod.ru>
2022-06-30 15:26:58 +0300

    deliver_build_script_job.sh update: support no-config option

--- automation/build_sh/deliver_build_scripts_job.sh
+++ automation/build_sh/deliver_build_scripts_job.sh
@@ -14,20 +14,32 @@ function deliver_build_scripts_job()
 	source log.sh
 	local log_prefix="[deliver_build_scripts_job]: "
 	[ -z "$1" ] && log_error "No directory provided" && return 1 || dir="$1"
-	[ ! -d "$dir" ] && log_error "Not existent directory provided: '$dir' relative to the invocation directory '${PWD}'" && exit
+	[ ! -d "$dir" ] && log_error "Not existent directory provided: '$dir' relative to the invocation directory '${PWD}'" && return
 
 	log "Deliver build scripts ..."
 
-	cp -a "$scripts_dir/build_sh/." "$dir"
+	local no_config=false
+	if [ ! -z "$2" ] && [ "$2" == "no-config" ]; then
+		log_info "'$2' option is passed. Will copy only main scripts"
+		local no_config=true
+	fi
+
+	if $no_config; then
+		cp "$scripts_dir/build_sh/build.sh" "$dir"
+		cp "$scripts_dir/build_sh/dependencies.sh" "$dir"
+		cp "$scripts_dir/build_sh/get_dependencies.sh" "$dir"
+	else
+		cp -a "$scripts_dir/build_sh/." "$dir"
+		rename $dir/build_config_example.sh build_config.sh
+		rename $dir/deps_config_example.sh deps_config.sh
+		rename $dir/deps_scenario_example.sh deps_scenario.sh
+		[ $? -ne 0 ] && log_error "Error during build scripts delivery" && return 5
+	fi
+
 	cp "$scripts_dir/include/log.sh" "$dir"
 	cp "$scripts_dir/include/os.sh" "$dir"
 	cp "$scripts_dir/include/file_utils.sh" "$dir"
-
-	[ $? -ne 0 ] && log_error "Error during build scripts delivery: $?" && exit 5
-	rename $dir/build_config_example.sh build_config.sh
-	rename $dir/deps_config_example.sh deps_config.sh
-	rename $dir/deps_scenario_example.sh deps_scenario.sh
-	[ $? -ne 0 ] && log_error "Error during build scripts delivery: $?" && exit 5
+	[ $? -ne 0 ] && log_error "Error during build scripts delivery" && return 6
 
 	log_success "Buld scripts delivered"
 }
