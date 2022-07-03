97b95296ba9dcef4b4fd904346e888d8a28e4a95
Alex <skorohodov-ad@narod.ru>
2022-07-03 21:35:13 +0300

    Update env.sh

--- automation/include/env.sh
+++ automation/include/env.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ENV_DIR=""
-
 # This function copies all the given files and directories contents
 # to the directory by the path from $ENV_DIR variable.
 # If the variable $ENV_DIR is not set then nothing happens
@@ -14,9 +12,9 @@ function env_include()
 
 	# do the work
 	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-	source $THIS_DIR/../automation_config.sh
-	source $scripts_dir/include/file_utils.sh
-	source $scripts_dir/include/log.sh
+	source $ENV_DIR/automation_config.sh
+	source $ENV_DIR/file_utils.sh
+	source $ENV_DIR/log.sh
 	local log_prefix="\033[1;37m	[env_include()]: "
 	local log_postfix="\033[0m"
 
@@ -59,50 +57,63 @@ function env_include()
 # every file from the target directory to 'env'.
 # If the includes list is given as the third argument it then also call env_include
 # for the given list
+
+function setup_global_variables()
+{
+	local log_prefix="[setup_global_variables()]: "
+	# log_info "setup_global_variables $1 $2"
+
+	[ -z "$1" ] && log_error "No directory provided" && return 1 || ENV_TARGET_DIR=$(dir_full_path "$1") # ENV_TARGET_DIR - the invocable script directory
+	[ ! -z "$2" ] && local where_dir=$(dir_full_path "$2") || local where_dir="$ENV_TARGET_DIR" # where we gonna create the environment
+
+	[ -z "$where_dir" ] && log_error "where_dir has not been set" && return 2
+	log_info "setup_environment in '$where_dir'"
+
+	# set the global ENV_DIR variable
+	ENV_DIR="$where_dir/env"
+
+	log "ENV_DIR: $ENV_DIR"
+	log "ENV_TARGET_DIR: $ENV_TARGET_DIR"
+	# check the processed directories paths are not empty
+	[ -z "$ENV_TARGET_DIR" ] && log_error "ENV_TARGET_DIR has not been set" && return 3
+	[ -z "$ENV_DIR" ] && log_error "ENV_DIR has not been set" && return 3
+	
+	return 0
+}
+
 function setup_environment()
 {
 	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 	source $THIS_DIR/../automation_config.sh
 	source $scripts_dir/include/file_utils.sh
 	source $scripts_dir/include/log.sh
-	local log_prefix="\033[1;37m	[setup_environment()]: "
-	local log_postfix="\033[0m"
+	local log_prefix="[setup_environment()]: "
 
 	# arguments 
-	[ -z "$1" ] && return 1 || ENV_TARGET_DIR=$(dir_full_path "$1") # ENV_TARGET_DIR
-	[ ! -z "$2" ] && local where_dir=$(dir_full_path "$2") || local where_dir="$ENV_TARGET_DIR" # where
-	[ ! -z "$3" ] && local includes=${@:3} #includes
-
+	[ -z "$1" ] && log_error "No directory provided" && return 1
 	# check the directories exists
-	[ ! -d "$1" ] && log "No such directory passed in the first argument: '$1'" && return 2
+	[ ! -d "$1" ] && log_error "Not existent directory passed in the first argument: '$1'" && return 2
 
-	# check the processed directories paths are not empty
-	[ -z "$ENV_TARGET_DIR" ] && return 3
-	[ -z "$where_dir" ] && return 4
+	setup_global_variables $@
+	[ $? -ne 0 ] && log_error "Error while calling setup_global_variables in '$1'" && return 3
 
 	# do the work
-	log "setup_environment in '$where_dir'"
+	log_info "ENV_DIR: $ENV_DIR"
 
-	local target_name=$(basename $ENV_TARGET_DIR)
-	local env_dir="$where_dir/env"
-
-	# set the global ENV_DIR variable
-	ENV_DIR=$env_dir
-
-	log "ENV_DIR: $ENV_DIR"
-
-	[ -d "$env_dir" ] && rm -rf $env_dir
-	mkdir -p $env_dir
-	# [ ! -d "$env_dir" ] && mkdir -p $env_dir
+	[ -d "$ENV_DIR" ] && rm -rf $ENV_DIR
+	mkdir -p $ENV_DIR
+	# [ ! -d "$ENV_DIR" ] && mkdir -p $ENV_DIR
 
 	# copy some essential dependencies
-	cp "$automation_dir/automation_config.sh" "$env_dir/"
-	cp "$scripts_dir/include/log.sh" "$env_dir/"
-	cp "$automation_dir/run_local.sh" "$env_dir/"
+	cp "$automation_dir/automation_config.sh" "$ENV_DIR/"
+	cp "$scripts_dir/include/log.sh" "$ENV_DIR/"
+	cp "$scripts_dir/include/file_utils.sh" "$ENV_DIR/"
+	cp "$automation_dir/run_local.sh" "$ENV_DIR/"
 	
 	# copy include directories content to the env directory
+	[ ! -z "$3" ] && local includes=${@:3} #includes
 	[ ! -z "$includes" ] && env_include ${includes[@]}
 
 	# copy the target directory content into the env
-	cp $ENV_TARGET_DIR/* "$env_dir/"
+	cp $ENV_TARGET_DIR/* "$ENV_DIR/"
 }
