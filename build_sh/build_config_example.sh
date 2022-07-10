971cd43bde1031d461f46bc3851421bc44811fee
skalexey <skorohodov-ad@narod.ru>
2022-07-10 19:01:35 +0300

    Update build scripts: support release config, proper variables overriding with a config, update external_config.sh

--- build_sh/build_config_example.sh
+++ build_sh/build_config_example.sh
@@ -6,5 +6,4 @@ buildFolderPrefix="Build"
 extraArg=" -DDEPS=${depsLocation} -DINCLUDES=${HOME}/Projects/Utils/include"
 extraArgWin=$extraArg
 extraArgMac=$extraArg
-buildConfig="Debug"
 logArg=" -DLOG_ON=ON"
--- build_sh/external_config_example.sh
+++ build_sh/external_config_example.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
-
-export netlib_asio_path_win="C:/lib/asio-1.22.1/include"
-export netlib_asio_path_arg_win=" -DASIO_PATH=$netlib_asio_path_win"
-export netlib_asio_path_mac="~/lib/asio-1.22.1/include"
-export netlib_asio_path_arg_mac=" -DASIO_PATH=$netlib_asio_path_mac"
+export {TPL_NAME}_deps="${HOME}/Projects"
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $THIS_DIR/os.sh
+
 if is_windows; then
-	export netlib_asio_path=$netlib_asio_path_win
+	export {TPL_NAME}_asio_path="C:/lib/asio-1.22.1/include"
 else
-	export netlib_asio_path=$netlib_asio_path_mac
+	export {TPL_NAME}_asio_path="~/lib/asio-1.22.1/include"
 fi
