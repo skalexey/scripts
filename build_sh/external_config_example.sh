4bea5e524eacfe99dafc3015c8eca77d3ee3248f
skalexey <skorohodov-ad@narod.ru>
2022-07-15 09:25:40 +0300

    Update external_config.sh.

--- build_sh/external_config_example.sh
+++ build_sh/external_config_example.sh
@@ -1,10 +1,11 @@
-export {TPL_NAME}_deps="${HOME}/Projects"
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $THIS_DIR/os.sh
 
 if is_windows; then
-	export {TPL_NAME}_asio_path="C:/lib/asio-1.22.1/include"
+	export {TPL_NAME}_deps="${HOME}/Projects"
+	# export {TPL_NAME}_asio_path="C:/lib/asio-1.22.1/include"
 else
-	export {TPL_NAME}_asio_path="~/lib/asio-1.22.1/include"
+	export {TPL_NAME}_deps="${HOME}/Projects"
+	# export {TPL_NAME}_asio_path="~/lib/asio-1.22.1/include"
 fi
