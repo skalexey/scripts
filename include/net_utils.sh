eef4b80e203d32bd0fde67c653a1f630e25bbca0
skalexey <skorohodov-ad@narod.ru>
2022-12-04 18:52:54 +0100

    net_utils: Try to use curl when there is no wget installed. Support tar.gz archives auto extracting.

--- include/net_utils.sh
+++ include/net_utils.sh
@@ -17,8 +17,10 @@ function direct_download()
 {
 	[ -z "$2" ] && local o="dwl" || local o="$2"
 	wget $1 -O "$o"
+	[ $? -ne 0 ] && curl -L $1 -o "$o"
 	[ $? -ne 0 ] && echo "Download error" && return 1
-	if check_download_type "tar.bz" $@; then
+
+	if check_download_type "tar.bz" || check_download_type "tar.gz" $@; then
 		tar -xvf "$o"
 		[ $? -ne 0 ] && echo "Tar bz extraction error" && return 2
 		rm "$o"
