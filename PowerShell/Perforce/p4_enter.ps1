da57a8351e877b0e719ab9928b1735b6500567df
Alex <biggun@writeme.com>
2023-11-15 20:48:51 +0100

    p4_enter script fixes.

--- PowerShell/Perforce/p4_enter.ps1
+++ PowerShell/Perforce/p4_enter.ps1
@@ -1,6 +1,6 @@
 
 
-if ($args.Length -ne 2) {
+if ($args.Length -lt 2) {
     Write-Error -Message "Usage: p4_enter.ps1 <user> <workspace_name>" -ErrorAction Stop
 }
 
@@ -11,11 +11,13 @@ p4 set P4USER=$user
 p4 set P4CLIENT=$workspace
 
 if ($args.Length -ge 3) {
-    p4 set P4PORT=$args[2]
+    $addr = $args[2]
+    p4 set P4PORT=$addr
 }
 
 if ($args.Length -ge 4) {
-    p4 set P4PASSWD=$args[3]
+    $hash = $args[3]
+    p4 set P4PASSWD=$hash
 }
 
 p4 login -a
