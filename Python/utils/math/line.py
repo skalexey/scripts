9907b0d5758b55099830894a2ac66daf46c711e5
skalexey <skorohodov-ad@narod.ru>
2024-04-19 20:21:07 +0200

    Line.vec()

--- Python/utils/math/line.py
+++ Python/utils/math/line.py
@@ -42,4 +42,7 @@ class Line():
 			((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)) / denominator,
 			((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) / denominator
 		)
+	
+	def vec(self):
+		return self.points[0] - self.points[1]
 		
