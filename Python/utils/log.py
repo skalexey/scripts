97aa8cc3abe7e865adccd2290837ebf5a71da61a
skalexey <skorohodov-ad@narod.ru>
2024-05-04 11:56:01 +0200

    Log time and level.

--- Python/utils/log.py
+++ Python/utils/log.py
@@ -9,30 +9,57 @@ class LogLevel:
 	WARNING = 3
 	ERROR = 4
 	CRITICAL = 5
+	PRINT = 6
+
+	def sign(level):
+		if level == LogLevel.PRINT:
+			return None
+		if level == LogLevel.PRINT:
+			return "P"
+		if level == LogLevel.VERBOSE:
+			return "V"
+		if level == LogLevel.DEBUG:
+			return "D"
+		if level == LogLevel.INFO:
+			return "I"
+		if level == LogLevel.WARNING:
+			return "W"
+		if level == LogLevel.ERROR:
+			return "E"
+		if level == LogLevel.CRITICAL:
+			return "C"
+		raise ValueError(f"Unknown log level when calling LogLevel.sign({level})")
 
 def set_log_level(level):
 	print(f"Setting log level to {level}")
 	global log_level
 	log_level = level
 
-def log(level, message):
+# Variadic arguments
+def log(message, level=LogLevel.PRINT):
 	if level >= log_level:
-		print(message)
+		# Print the log level,time (hour, minute, second, and microsecond), and the message
+		from datetime import datetime
+		now = datetime.now()
+		current_time = now.strftime("%H:%M:%S.%f")
+		level_sign = LogLevel.sign(level)
+		level_prefix = f"[{level_sign}] " if level_sign is not None else "    "
+		print(f"{level_prefix}[{current_time}] {message}")
 
-def print_debug(message):
-	log(LogLevel.DEBUG, message)
+def log_debug(message):
+	log(message, LogLevel.DEBUG)
 
-def print_info(message):
-	log(LogLevel.INFO, message)
+def log_info(message):
+	log(message, LogLevel.INFO)
 
-def print_warning(message):
-	log(LogLevel.WARNING, message)
+def log_warning(message):
+	log(message, LogLevel.WARNING)
 
-def print_error(message):
-	log(LogLevel.ERROR, message)
+def log_error(message):
+	log(message, LogLevel.ERROR)
 
-def print_critical(message):
-	log(LogLevel.CRITICAL, message)
+def log_critical(message):
+	log(message, LogLevel.CRITICAL)
 
-def print_verbose(message):
-	log(LogLevel.VERBOSE, message)
+def log_verbose(message):
+	log(message, LogLevel.VERBOSE)
