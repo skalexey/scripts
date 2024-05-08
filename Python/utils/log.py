59b93acf83e1cb40c6aea7e630469f22a38fc107
skalexey <skorohodov-ad@narod.ru>
2024-05-08 20:03:46 +0200

    logger + monolite interface

--- Python/utils/log.py
+++ Python/utils/log.py
@@ -1,7 +1,5 @@
 # Allow calls like this: set_log_level(LogLevel.DEBUG)
 
-log_level = 0
-
 class LogLevel:
 	VERBOSE = 0
 	DEBUG = 1
@@ -30,36 +28,93 @@ class LogLevel:
 			return "C"
 		raise ValueError(f"Unknown log level when calling LogLevel.sign({level})")
 
-def set_log_level(level):
-	print(f"Setting log level to {level}")
-	global log_level
-	log_level = level
+class Logger:
+	def __init__(self):
+		self.log_level = 0
+		self.log_title = None
 
-# Variadic arguments
+	def set_log_title(self, title):
+		self.log_title = title
+
+	def set_log_level(self, level):
+		print(f"Setting log level to {level}")
+		self.log_level = level
+
+	# Variadic arguments
+	def log(self, message, level=LogLevel.PRINT):
+		if level >= self.log_level:
+			# Print the log level,time (hour, minute, second, and microsecond), and the message
+			from datetime import datetime
+			now = datetime.now()
+			current_time = now.strftime("%H:%M:%S.%f")
+			level_sign = LogLevel.sign(level)
+			level_prefix = f"[{level_sign}] " if level_sign is not None else "    "
+			log_title_addition = f"[{self.log_title}]: " if self.log_title is not None else ""
+			print(f"{level_prefix}[{current_time}] {log_title_addition}{message}")
+
+	def log_debug(self, message):
+		self.log(message, LogLevel.DEBUG)
+
+	def log_info(self, message):
+		self.log(message, LogLevel.INFO)
+
+	def log_warning(self, message):
+		self.log(message, LogLevel.WARNING)
+
+	def log_error(self, message):
+		self.log(message, LogLevel.ERROR)
+
+	def log_critical(self, message):
+		self.log(message, LogLevel.CRITICAL)
+
+	def log_verbose(self, message):
+		self.log(message, LogLevel.VERBOSE)
+
+# Discouraged global monolite interface.
+# Prefer crating a logger object in the calling module.
+loggers = {}
+
+def logger():
+	import inspect
+	# frame = inspect.currentframe().f_back
+	# module = inspect.getmodule(frame)
+	stack = inspect.stack()
+	# Find the first module that doesn't end with log.py
+	# This is the module that called the logger
+	calling_module_name = None
+	for i in range(1, len(stack)):
+		if not stack[i].filename.endswith("log.py"):
+			calling_module_name = stack[i].filename
+			break
+	if calling_module_name is None:
+		raise ValueError("Could not find calling module")
+	if calling_module_name not in loggers:
+		loggers[calling_module_name] = Logger()
+	return loggers[calling_module_name]
+
+def set_log_title(title):
+	logger().set_log_title(title)
+
+def set_log_level(level):
+	logger().set_log_level(level)
+
 def log(message, level=LogLevel.PRINT):
-	if level >= log_level:
-		# Print the log level,time (hour, minute, second, and microsecond), and the message
-		from datetime import datetime
-		now = datetime.now()
-		current_time = now.strftime("%H:%M:%S.%f")
-		level_sign = LogLevel.sign(level)
-		level_prefix = f"[{level_sign}] " if level_sign is not None else "    "
-		print(f"{level_prefix}[{current_time}] {message}")
+	logger().log(message, level=LogLevel.PRINT)
 
 def log_debug(message):
-	log(message, LogLevel.DEBUG)
+	logger().log_debug(message)
 
 def log_info(message):
-	log(message, LogLevel.INFO)
+	logger().log_info(message)
 
 def log_warning(message):
-	log(message, LogLevel.WARNING)
+	logger().log_warning(message)
 
 def log_error(message):
-	log(message, LogLevel.ERROR)
+	logger().log_error(message)
 
 def log_critical(message):
-	log(message, LogLevel.CRITICAL)
+	logger().log_critical(message)
 
 def log_verbose(message):
-	log(message, LogLevel.VERBOSE)
+	logger().log_verbose(message)
