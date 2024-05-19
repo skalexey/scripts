49a894bd3d92e248c752d6c4625781fe80dd193c
skalexey <skorohodov-ad@narod.ru>
2024-05-19 19:21:15 +0200

    Range

---
+++ Python/utils/math/utility.py
@@ -0,0 +1,29 @@
+import math
+
+class Range:
+	def __init__(self, min, max = None):
+		self.data = (min, max if max is not None else min)
+	
+	def size(self):
+		return math.abs(self.data[1] - self.data[0])
+
+	def expand(self, min, max = None):
+		self.data[0] = min(min, self.min)
+		self.data[1] = max(max if max is not None else min, self.max)
+
+	@property
+	def min(self):
+		return self.data[0]
+
+	@property
+	def max(self):
+		return self.data[1]
+
+	def __contains__(self, item):
+		return self.min <= item <= self.max
+
+	def __repr__(self):
+		return f"Range({self.min}, {self.max})"
+	
+	def __getitem__(self, item):
+		return self.data[item]
