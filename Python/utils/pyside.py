6b2eb0ee0d1f464d0265b277911149d7c324e069
skalexey <skorohodov-ad@narod.ru>
2024-06-04 20:39:30 +0200

    pyside utils

--- Python/utils/pyside.py
+++ Python/utils/pyside.py
@@ -35,3 +35,46 @@ def select_data_file():
 def show_message(title, message):
 	logger.log_info(f"Show message: {title}, {message}")
 	QMessageBox.information(None, title, message)
+
+def create_slider_input_widget(parent_layout, label, min_value, max_value, default_value, on_changed, slider_fixed_width=None):
+	widget = QWidget()
+	layout = QHBoxLayout()
+	parent_layout.addWidget(widget)
+	widget.setLayout(layout)
+	label_widget = QLabel(label)
+	layout.addWidget(label_widget)
+	slider = QSlider(Qt.Horizontal)
+	slider.setMinimum(min_value)
+	slider.setMaximum(max_value)
+	slider.setValue(default_value)
+	if slider_fixed_width is not None:
+		slider.setFixedWidth(slider_fixed_width)
+	layout.addWidget(slider)
+	value_label = QLabel(str(default_value))
+	layout.addWidget(value_label)
+	def on_value_changed(value):
+		value_label.setText(str(value))
+		on_changed(value)
+	slider.valueChanged.connect(on_changed)
+	return widget, slider, value_label
+
+def create_line_input_widget(parent_layout, label, default_value=None, on_changed=None, line_fixed_width=None):
+	widget = QWidget()
+	layout = QHBoxLayout()
+	parent_layout.addWidget(widget)
+	widget.setLayout(layout)
+	label_widget = QLabel(label)
+	layout.addWidget(label_widget)
+	line_edit = QLineEdit()
+	if default_value is not None:
+		t = type(default_value)
+		assert(t is str or t is int or t is float or t is bool)
+		line_edit.setText(str(default_value))
+	if line_fixed_width is not None:
+		line_edit.setFixedWidth(line_fixed_width)
+	layout.addWidget(line_edit)
+	if on_changed is not None:
+		def on_text_changed(text):
+			on_changed(text)
+		line_edit.textChanged.connect(on_text_changed)
+	return widget, line_edit
