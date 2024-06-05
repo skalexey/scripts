c756b05ae76e047184e5b6ae6fc5fdab91a7bd87
skalexey <skorohodov-ad@narod.ru>
2024-06-05 20:56:35 +0200

    task_scheduler.py

--- Python/utils/task_scheduler.py
+++ Python/utils/task_scheduler.py
@@ -1,8 +1,12 @@
+# TODO: Wait for the task utilization after cancelling through a future
+
 import asyncio
 from collections import deque
 
 import utils.asyncio_utils
+from utils.log import *
 
+logger = Logger("task_scheduler")
 
 # This class runs any async function passed to it and returns a future that can be awaited on
 class TaskScheduler():
@@ -13,10 +17,10 @@ class TaskScheduler():
 		self._current_task_info = None
 
 	class TaskInfo:
-		def __init__(self, function, task = None, future=asyncio.Future()):
+		def __init__(self, function, task = None, future=None):
 			self.function = function
 			self.task = task
-			self.future = future
+			self.future = future or asyncio.Future()
 
 	def schedule_task(self, async_function, max_queue_size=0):
 		if 0 <= max_queue_size > len(self._queue) or self._current_task_info is None:
@@ -24,16 +28,25 @@ class TaskScheduler():
 			self._queue.append(task_info)
 			if self._current_task_info is None:
 				return self._run_next()
+			else:
+				return task_info.future
 		# Don't fit by queue size
 		return None
 
 	def wait_all_tasks(self):
 		for task_info in list(self._tasks.values()):
-			task_info.future.get_loop().run_until_complete(task_info.future)
+			try:
+				task_info.future.get_loop().run_until_complete(task_info.future)
+			except asyncio.CancelledError:
+				logger.log_info(f"Task has been cancelled (tas_info: {task_info})")
 
 	def wait(self, future):
 		future.get_loop().run_until_complete(future)
 	
+	def cancel_all_tasks(self):
+		self._queue.clear()
+		for task_info in list(self._tasks.values()):
+			task_info.task.cancel()
 	# Use this method in a loop if your application doesn't have an event loop running
 	def update(self, dt):
 		if len(self._tasks) > 0:
@@ -44,17 +57,17 @@ class TaskScheduler():
 
 	def _create_task_info(self, async_function):
 		task_info = self.TaskInfo(async_function)
-		self._tasks[async_function] = task_info
 		return task_info
 
 	def _run_next(self):
 		task_info = self._queue.popleft()
+		assert(task_info.task is None)
 		self._current_task_info = task_info
 		loop = utils.asyncio_utils.get_event_loop()
 		task = loop.create_task(task_info.function())
 		task_info.task = task
-		task.add_done_callback(self._set_result)
 		self._tasks[task] = task_info
+		task.add_done_callback(self._set_result)
 		future = task_info.future
 		def future_done(future):
 			if future.cancelled():
@@ -64,13 +77,14 @@ class TaskScheduler():
 
 	def _set_result(self, task):
 		task_info = self._tasks.pop(task)
+		assert(task_info == self._current_task_info)
 		task, future = task_info.task, task_info.future
 		if task.cancelled():
-			return
-		# assert not future.done()
-		if not future.done():
-			future.set_result(task.result())
+			if not future.done():
+				future.cancel()
+		else:
+			if not future.done():
+				future.set_result(task.result())
+		self._current_task_info = None
 		if len(self._queue) > 0:
 			self._run_next()
-		else:
-			self._current_task_info = None
