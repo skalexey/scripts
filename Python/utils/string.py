46667653a11cd9603cc9a81222661237d784c8f5
skalexey <skorohodov-ad@narod.ru>
2024-06-07 20:18:40 +0200

    Code cleaning

--- Python/utils/string.py
+++ Python/utils/string.py
@@ -13,14 +13,6 @@ def title(string, filler='=', length=80):
 	# Example: title("Hello", "=", 80) -> "================================== Hello =================================="
 	# Example: title("Hello", "-", 80) -> "---------------------------------- Hello ---------------------------------"
 	# Example: title("Hello", "=", 20) -> "======== Hello ========"
-	# Thecode:
-	# 1. Calculate the length of the filler on the left side
-	# 2. Calculate the length of the filler on the right side
-	# 3. Calculate the length of the filler on the left side
-	# 4. Calculate the length of the filler on the right side
-	# 5. Return the filler on the left side + the string + the filler on the right side
-	# The time complexity of this function is O(1)
-	# The space complexity of this function is O(1)
 	left_length = (length - len(string) - 2) // 2
 	right_length = length - len(string) - 2 - left_length
 	return f"{filler * left_length} {string} {filler * right_length}"
