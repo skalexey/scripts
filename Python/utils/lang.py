19aa6a2d765f08b279aefd90cdc75c79c2883493
skalexey <skorohodov-ad@narod.ru>
2024-06-10 17:16:52 +0200

    utils.lang.clear_resources

--- Python/utils/lang.py
+++ Python/utils/lang.py
@@ -2,3 +2,7 @@ def extract_self(bound_method):
 	if hasattr(bound_method, '__self__'):
 		return bound_method.__self__
 	return None
+
+def clear_resources(obj):
+	for attr in obj.__dict__:
+		obj.__dict__[attr] = None
