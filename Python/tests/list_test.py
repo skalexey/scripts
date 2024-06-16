63d4d49c8ba1bea03795174a299e616a513a9fe3
skalexey <skorohodov-ad@narod.ru>
2024-06-16 19:33:27 +0200

    Tests

--- Python/tests/list_test.py
+++ Python/tests/list_test.py
@@ -1,2 +1,23 @@
-elements = [1, 2, 3, 4, 5]
-print(elements.length)
+from test import *
+
+
+def length_test():
+	log(title("List Length Test"))
+	elements = [1, 2, 3, 4, 5]
+	log(elements)
+	log("Length of elements: " + str(len(elements)))
+	log(title("End of List Length Test"))
+
+def empty_list_test():
+	log(title("Empty List Test"))
+	elements = []
+	log(elements or 'Empty')
+	log(title("End of Empty List Test"))
+
+def test():
+	log(title("List Test"))
+	length_test()
+	empty_list_test()
+	log(title("End of List Test"))
+
+run()
---
+++ Python/tests/sqlite_utils_test.py
@@ -0,0 +1,19 @@
+from test import *
+
+import utils.sqlite
+
+
+def test_create_or_alter_table_from_json():
+	# Example usage:
+	json_data = {
+		"name": "John Doe",
+		"age": 30,
+		"city": "New York"
+	}
+	primary_keys = ["name"]  # Example primary keys
+	utils.sqlite.create_or_alter_table_from_json(json_data, 'users', 'example.db', primary_keys)
+
+def test():
+	log(title("SQLite Test"))
+	test_create_or_alter_table_from_json()
+	log(title("End of SQLite Test"))
