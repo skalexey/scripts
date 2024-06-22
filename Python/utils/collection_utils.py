640f2fe8d4e1acb107d47628fad41e30ac3d0eb4
skalexey <skorohodov-ad@narod.ru>
2024-06-22 20:22:02 +0200

    collection_utils: update_existing, add_to_set_field, as_set

--- Python/utils/collection_utils.py
+++ Python/utils/collection_utils.py
@@ -15,11 +15,38 @@ else:
 # def find_first(iterable, condition):
 # 	return next((x for x in iterable if condition(x)), None)
 
-def fetch_dict(data, keys=None, ignore=[]):
+def fetch_dict(data, keys=None, ignore=set()):
 	_keys = keys or data.keys()
-	return {key: data[key] for key in _keys if key in data and key not in ignore}
+	result = {}
+	for key in _keys:
+		if key not in ignore:
+			if key in data:
+				result[key] = data[key]
+	return result
 
-def fill_dict(target, source, ignore=[]):
+def fill_dict(target, source, ignore={}):
 	for key in target.keys():
 		if key not in ignore:
 			target[key] = source[key]
+
+def update_existing(target, source):
+	for key in target.keys():
+		if key in source:
+			target[key] = source[key]
+
+def add_to_set_field(target, key, value):
+	current = target.get(key, None)
+	if current is None:
+		target[key] = value
+	else:
+		if isinstance(current, set):
+			current.add(value)
+		else:
+			target[key] = {current, value}
+
+def as_set(value):
+	if value is None:
+		return set()
+	if isinstance(value, set):
+		return value
+	return {value}
