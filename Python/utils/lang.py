89dc995e0ece6b514eed85c2dd8d945c84ceba00
skalexey <skorohodov-ad@narod.ru>
2024-06-22 20:25:02 +0200

    utils.lang: is_primitive, defnew

--- Python/utils/lang.py
+++ Python/utils/lang.py
@@ -1,3 +1,12 @@
+import inspect
+
+import utils.inspect_utils as inspect_utils
+import utils.ordered_dict
+
+
+def is_primitive(value):
+	return isinstance(value, (int, float, str, bool, bytes))
+
 def extract_self(bound_method):
 	if hasattr(bound_method, '__self__'):
 		return bound_method.__self__
@@ -6,3 +15,18 @@ def extract_self(bound_method):
 def clear_resources(obj):
 	for attr in obj.__dict__:
 		obj.__dict__[attr] = None
+
+class DefaultNew:
+	def __init__(self, callable, *args, **kwargs):
+		self._callable = callable
+		self._args = args
+		self._kwargs = kwargs
+
+	def __get__(self, instance, owner):
+		if instance is None:
+			return self
+		else:
+			return self._callable(*self._args, **self._kwargs)
+
+def defnew(callable, *args, **kwargs):
+	return DefaultNew(callable, *args, **kwargs)
