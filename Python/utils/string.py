a674154689fffd4a0464789b3f939feca933cab9
skalexey <skorohodov-ad@narod.ru>
2024-06-22 20:27:18 +0200

    utils.string.is_datetime

--- Python/utils/string.py
+++ Python/utils/string.py
@@ -7,3 +7,7 @@ def to_camel_case(snake_str):
 
 def to_snake_case(camel_str):
 	return re.sub(r'(?<!^)(?=[A-Z])', '_', camel_str).lower()
+
+def is_datetime(string):
+	pattern = r'^\d{4}-\d{2}-\d{2}[ T]\d{2}:\d{2}(:\d{2}(\.\d{1,6})?)?(Z|[\+\-]\d{2}:\d{2})?$'
+	return bool(re.match(pattern, string))
