277b201c746d6651b69b83ced78f98e264d5d80c
skalexey <skorohodov-ad@narod.ru>
2024-06-24 19:51:11 +0200

    OrderedSet, OrderedDict

--- Python/utils/ordered_dict.py
+++ Python/utils/ordered_dict.py
@@ -1,58 +1,32 @@
-import collections
-from functools import total_ordering
-
-from utils.serialize import Serializable
+from utils.ordered_set import OrderedSet
 
 
-@total_ordering
-class OrderedDict(Serializable):
-	def __init__(self, list=None, keys=None):
+class OrderedDict(OrderedSet):
+	def __init__(self, list=None, keys=None, **kwargs):
+		super().__init__(keys, **kwargs) # Assign manually, since assigning algorithm uses collection, an can be configured working with OrderedDict
 		self._list = list or []
-		self._keys = keys or []
-		self._dict = {key: index for index, key in enumerate(self._keys)}
-		super().__init__(assign_attrs=False) # Assign manually, since assigning algorithm uses collection, an can be configured with OrderedDict
 
 	def _serialize_mapping(self):
 		mapping = super()._serialize_mapping()
 		mapping.update({
-			"list": "_list",
-			"keys": "_keys"
+			"list": "_list"
 		})
 		return mapping
 
-	def index(self, key):
-		index = self._dict.get(key)
-		if index is None:
-			return -1
-		return index
-	
 	def insert(self, index, key, value):
-		if key in self._dict:
-			raise KeyError(f"Key '{key}' already exists")
-		if index < len(self._list):
-			for k, v in self._dict.items():
-				if v >= index:
-					self._dict[k] += 1
-		self._dict[key] = index
+		super().insert(index, key)
 		self._list.insert(index, value)
-		self._keys.insert(index, key)
 
 	def set_at(self, index, key, value):
-		if index < 0 or index >= len(self._list):
-			raise IndexError(f"OrderedDict: Index out of range({index})")
-		self._keys[index] = key
+		super().set_at(index, key)
 		self._list[index] = value
-		self._dict[key] = index
 
 	def __setitem__(self, key, value):
-		index = self._dict.get(key)
-		if index is None:
-			index = len(self._list)
+		result = super().add(key)
+		if result is True:
 			self._list.append(value)
-			self._keys.append(key)
-			self._dict[key] = index
 		else:
-			self._list[index] = value
+			self._list[result] = value
 
 	def add(self, key, value):
 		if key not in self._dict:
@@ -66,16 +40,8 @@ class OrderedDict(Serializable):
 		return self[key]
 
 	def update(self, other):
-		if isinstance(other, OrderedDict):
-			self._dict.update(other._dict)
-			self._list.extend(other._list)
-			self._keys.extend(other.keys)
-		elif isinstance(other, (dict, collections.OrderedDict)):
-			for key, value in other.items():
-				self[key] = value
-		else:
-			for key, value in other:
-				self[key] = value
+		for key, value in other.items():
+			self[key] = value
 
 	def copy(self):
 		new = OrderedDict()
@@ -106,27 +72,24 @@ class OrderedDict(Serializable):
 		del self._dict[key]
 		return key, value
 
-	def pop(self, key, default=None):
-		index = self._dict.get(key)
-		if index is None:
-			return default
-		del self._keys[index]
-		del self._dict[key]
-		value = self._list.pop(index)
-		for key, value in self._dict.items():
-			if value > index:
-				self._dict[key] -= 1
-		return value
-
-	def keys(self):
-		return self._keys
+	def pop(self, *args):
+		len_before = len(self)
+		pop_result = super().pop(*args)
+		if len_before == len(self):
+			assert len(args) == 2
+			assert pop_result is args[1]
+			return pop_result # Default value
+		return self._list.pop(pop_result)
 
 	def values(self):
 		return self._list
 
 	def items(self):
-		return zip(self._keys, self._list)
-	
+		return self.__iter__()
+
+	def keys(self):
+		return self._keys
+
 	def dictionary(self):
 		result = {}
 		for key, value in self.items():
@@ -134,56 +97,43 @@ class OrderedDict(Serializable):
 		return result
 
 	def clear(self):
-		self._dict.clear()
+		super().clear()
 		self._list.clear()
-		self._keys.clear()
 
 	def get(self, key, default=None):
-		index = self._dict.get(key)
-		return self._list[index] if index is not None else default
+		idx = self.index(key)
+		return self._list[idx] if idx is not None else default
 
 	def __getitem__(self, key):
 		if isinstance(key, slice):
 			keys = self._keys[key]
 			return [self.__getitem__(k) for k in keys]
 		else:
-			index = self._dict[key]
-			return self._list[index]
+			idx = self.index(key)
+			if idx is None:
+				raise KeyError(key)
+			return self._list[idx]
 
-	def __delitem__(self, key):
-		self.pop(key)
-
-	def remove(self, key):
-		del self[key]
-
-	def remove_at(self, index):
-		key = self._keys[index]
-		del self[key]
-		
 	def __iter__(self):
-		for index in range(len(self._list)):
+		for key, value in zip(self._keys, self._list):
+			yield key, value
+
+	def __reversed__(self):
+		for index in range(len(self._list) - 1, -1, -1):
 			key = self._keys[index]
 			value = self._list[index]
 			yield key, value
 
-	def __len__(self):
-		return len(self._list)
-
-	def __contains__(self, key):
-		return key in self._dict
-
 	def __str__(self):
-		return str(self._dict)
+		return self.__repr__()
 
 	def __repr__(self):
-		return f"utils.OrderedDict({self._dict})"
+		# Contents in the format k->i: v
+		contents_str = ", ".join([f"{key}->{index}: {value}" for key, index, value in zip(self._keys, range(len(self._list)), self._list)])
+		return f"utils.OrderedDict({contents_str})"
 
 	def __eq__(self, other):
-		if not isinstance(other, OrderedDict):
-			return False
-		if len(self) != len(other):
-			return False
-		if self._dict != other._dict:
+		if not super().__eq__(other):
 			return False
 		if self._list != other._list:
 			return False
@@ -197,50 +147,15 @@ class OrderedDict(Serializable):
 		# It will raise a builtin exception
 		return self._dict < other
 
-	def __add__(self, other):
-		new = self.copy()
-		new.update(other)
-		return new
-
-	def __iadd__(self, other):
-		self.update(other)
-		return self
-	
-	def __sub__(self, other):
-		new = self.copy()
-		for key in other:
-			del new[key]
-		return new
-	
-	def __isub__(self, other):
-		for key in other:
-			del self[key]
-		return self
-	
-	def __or__(self, other):
-		new = self.copy()
-		new.update(other)
-		return new
-	
-	def __ior__(self, other):
-		self.update(other)
-		return self
-	
 	def __and__(self, other):
-		new = OrderedDict()
+		new = self.__class__()
 		for key in self:
 			if key in other:
-				new[key] = self[key]
+				new.add(key)
 		return new
 	
-	def __iand__(self, other):
-		for key in self:
-			if key not in other:
-				del self[key]
-		return self
-	
 	def __xor__(self, other):
-		new = OrderedDict()
+		new = self.__class__()
 		for key in self:
 			if key not in other:
 				new[key] = self[key]
@@ -257,5 +172,3 @@ class OrderedDict(Serializable):
 				self[key] = other[key]
 		return self
 	
-	def __reversed__(self):
-		return reversed(self._list)
---
+++ Python/utils/ordered_set.py
@@ -0,0 +1,305 @@
+import collections
+from functools import total_ordering
+
+import utils.function
+from utils.serialize.serializable import Serializable
+
+
+@total_ordering
+class OrderedSet(Serializable):
+	def __init__(self, keys=None):
+		self._keys = keys or []
+		self._dict = {key: index for index, key in enumerate(self._keys)}
+		super().__init__(assign_attrs=False) # Assign manually, since assigning algorithm uses collection, an can be configured working with OrderedDict
+
+	def _serialize_mapping(self):
+		mapping = super()._serialize_mapping()
+		mapping.update({
+			"keys": "_keys"
+		})
+		return mapping
+
+	def at(self, index, default=None):
+		return self._keys[index] if index < len(self._keys) and index >= 0 else default
+
+	def index(self, key):
+		return self._dict.get(key)
+	
+	def insert(self, index, key):
+		if key in self._dict:
+			raise KeyError(utils.function.msg(f"Key '{key}' already exists"))
+		if index < len(self._keys):
+			for k, v in self._dict.items():
+				if v >= index:
+					self._dict[k] += 1
+		self._dict[key] = index
+		self._keys.insert(index, key)
+
+	def set_at(self, index, key, value):
+		if index < 0 or index >= len(self._keys):
+			raise IndexError(utils.function.msg(f"Index out of range({index})"))
+		self._keys[index] = key
+		self._dict[key] = index
+
+	def add(self, key):
+		idx = self.index(key)
+		if idx is None:
+			idx = len(self._keys)
+			self._keys.append(key)
+			self._dict[key] = idx
+			return True
+		return idx
+
+	def update(self, other):
+		for key in other:
+			self.add(key)
+
+	def copy(self):
+		new = OrderedSet()
+		new._dict = self._dict.copy()
+		new._keys = self._keys.copy()
+		return new
+
+	def sort(self, pred):
+		self._keys.sort(key=pred)
+		for index, key in enumerate(self._keys):
+			self._dict[key] = index
+
+	# Returns an index of the key if it exists.
+	# If the key is not present in the set, the second argument is returned if provided, otherwise a KeyError is raised.
+	def _pop(self, *args):
+		if len(args) > 2:
+			raise TypeError(f"pop expected at most 1 arguments, got {len(args)}")
+		if len(args) == 0:
+			if self.empty():
+				raise KeyError("Pop from an empty OrderedSet")
+			index_to_remove = len(self._keys) - 1
+			key_to_remove = self._keys[index_to_remove]
+			# self.remove_at(index_to_remove)
+			self._keys.pop()
+			del self._dict[key_to_remove]
+			return key_to_remove, index_to_remove
+		default = args[0] if args else None
+		len_before = len(self._dict)
+		pop_result = self._dict.pop(*args) # Raise KeyError if key is not present and the default value is not provided
+		if len_before == len(self._dict):
+			return default, pop_result
+		index = pop_result
+		val = self._keys.pop(index)
+		for k, v in self._dict.items():
+			if v > index:
+				self._dict[k] -= 1
+		return val, index
+
+	def pop(self, *args):
+		result = self._pop(*args)
+		return result[0] if len(args) == 0 else result[1]
+
+	def clear(self):
+		self._dict.clear()
+		self._keys.clear()
+
+	def __delitem__(self, key):
+		return self.remove(key)
+
+	def remove(self, key):
+		return self.pop(key)
+
+	def remove_at(self, index):
+		key = self._keys[index]
+		self.remove(key)
+		
+	def empty(self):
+		return not bool(self)
+
+	def __bool__(self):
+		return bool(self._keys)
+
+	def __iter__(self):
+		return iter(self._keys)
+	
+	def __reversed__(self):
+		return reversed(self._keys)
+
+	def __len__(self):
+		return len(self._keys)
+
+	def __contains__(self, key):
+		return key in self._dict
+
+	def __str__(self):
+		return self.__repr__()
+
+	def __repr__(self):
+		return f"utils.OrderedSet({self._keys})"
+
+	def __eq__(self, other):
+		if not isinstance(other, OrderedSet):
+			return False
+		if len(self) != len(other):
+			return False
+		if self._dict != other._dict:
+			return False
+		return True
+
+	def __lt__(self, other):
+		if isinstance(other, OrderedSet):
+			return self._dict < other._dict
+		if isinstance(other, list):
+			return self._keys < other
+		# It will raise a builtin exception
+		return self._dict < other
+
+	def __add__(self, other):
+		new = self.copy()
+		new.update(other)
+		return new
+
+	def __iadd__(self, other):
+		self.update(other)
+		return self
+	
+	def __sub__(self, other):
+		new = self.copy()
+		for key in other:
+			if key in new:
+				del new[key]
+		return new
+	
+	def __isub__(self, other):
+		for key in other:
+			if key in self:
+				del self[key]
+		return self
+	
+	def __or__(self, other):
+		new = self.copy()
+		new.update(other)
+		return new
+	
+	def __ior__(self, other):
+		self.update(other)
+		return self
+	
+	def __and__(self, other):
+		# Create using a list comprehension
+		new = self.__class__([key for key in self if key in other])
+		return new
+	
+	def __iand__(self, other):
+		for key in self:
+			if key not in other:
+				del self[key]
+		return self
+	
+	def __xor__(self, other):
+		new = OrderedSet()
+		for key in self:
+			if key not in other:
+				new.add(key)
+		for key in other:
+			if key not in self:
+				new.add(key)
+		return new
+	
+	def __ixor__(self, other):
+		for key in other:
+			if key in self:
+				del self[key]
+			else:
+				self.add(key)
+		return self
+	
+# if main 
+if __name__ == "__main__":
+	from utils.log.logger import Logger
+	log = Logger()
+	def creation_test():
+		log.expr("s1 = OrderedSet()")
+		log.expr_and_val("s1")
+		log.expr("s2 = OrderedSet(['a', 'b', 'c'])")
+		log.expr_and_val("s2")
+		log.expr("s3 = OrderedSet({'a', 'b', 'c'})")
+		log.expr_and_val("s3")
+		log.expr("s4 = OrderedSet(s2)")
+		log.expr_and_val("s4")
+		log.expr("s5 = OrderedSet(s3)")
+		log.expr_and_val("s5")
+		
+	def test_pop():
+		def test1():
+			ordered_set = OrderedSet(['a', 'b', 'c'])
+			assert ordered_set._pop() == ('c', 2)
+			assert ordered_set._pop() == ('b', 1)
+			assert ordered_set._pop() == ('a', 0)
+			ex = None
+			try:
+				ordered_set.pop()
+			except KeyError as e:
+				ex = e
+				log.expr_and_val("e")
+			assert ex is not None
+
+		def test2():
+			os = OrderedSet(['a', 'b', 'c'])
+			assert os._pop('a') == ('a', 0)
+			assert os._pop('b') == ('b', 0)
+			assert os._pop('c') == ('c', 0)
+			os = OrderedSet(['a', 'b', 'c'])
+			assert os.pop('a') == 0
+			assert os.pop('b') == 0
+			assert os.pop('c') == 0
+			ex = None
+			try:
+				os.pop('a')
+			except KeyError as e:
+				ex = e
+				log.expr_and_val("e")
+			assert ex is not None
+
+		def test3():
+			os = OrderedSet(['a', 'b', 'c'])
+			assert os.pop('a', 'd') == 0
+			assert os.pop('b', 'd') == 0
+			assert os.pop('c', 'd') == 0
+			assert os.pop('d', 'd') == 'd'
+			assert os.pop('e', 'd') == 'd'
+
+			os = OrderedSet(['a', 'b', 'c'])
+			assert os._pop('a', 'd') == ('a', 0)
+			assert os._pop('b', 'd') == ('b', 0)
+			assert os._pop('c', 'd') == ('c', 0)
+			assert os._pop('d', 'd') == ('d', 'd')
+			assert os._pop('e', 'd') == ('e', 'd')
+
+		test1()
+		test2()
+		test3()
+
+	def test_sub():
+		def test1():
+			os1 = OrderedSet(['a', 'b', 'c'])
+			os2 = OrderedSet(['a', 'b'])
+			assert os1 - os2 == OrderedSet(['c'])
+			assert os2 - os1 == OrderedSet()
+			assert os1 - OrderedSet() == os1
+			assert os1 - OrderedSet(['a', 'b', 'c']) == OrderedSet()
+			assert os1 - OrderedSet(['a', 'b']) == OrderedSet(['c'])
+			assert os1 - OrderedSet(['a', 'c']) == OrderedSet(['b'])
+			assert os1 - OrderedSet(['b', 'c']) == OrderedSet(['a'])
+			assert os1 - OrderedSet(['a']) == OrderedSet(['b', 'c'])
+			assert os1 - OrderedSet(['b']) == OrderedSet(['a', 'c'])
+			assert os1 - OrderedSet(['c']) == OrderedSet(['a', 'b'])
+
+		def test2():
+			os1 = OrderedSet(['a', 'b', 'c'])
+			os2 = OrderedSet(['a', 'b'])
+			os1 -= os2
+			assert os1 == OrderedSet(['c'])
+			os2 -= os1
+			assert os2 == OrderedSet(['a', 'b'])
+
+		test1()
+		test2()
+	test_pop()
+	test_sub()
