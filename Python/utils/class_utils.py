a9d33e87fda8208f66bf122f49d93ee74ec80dad
skalexey <skorohodov-ad@narod.ru>
2024-06-24 19:58:43 +0200

    utils.file.backup

--- Python/utils/class_utils.py
+++ Python/utils/class_utils.py
@@ -1,2 +1,36 @@
-def class_path(cls):
+import utils.inspect_utils
+
+
+def class_path(cls_or_name):
+	if isinstance(cls_or_name, str):
+		frame = utils.inspect_utils.caller_frame()
+		cls = find_class(cls_or_name, globals=frame.f_globals)
+		if cls is None:
+			raise ValueError(f"Couldn't find class '{cls_or_name}' in globals() on the call level nor in __builtins__")
+		return cls
+	else:
+		return f"{cls_or_name.__module__}.{cls_or_name.__name__}"
 	return f"{cls.__module__}.{cls.__name__}"
+
+def class_name(cls_or_path):
+	if isinstance(cls_or_path, str):
+		return cls_or_path.split('.')[-1]
+	else:
+		cls = cls_or_path
+
+def find_class(class_path_or_name, globals=None): # e.g. 'module.submodule.ClassName'
+	classname = class_name(class_path_or_name)
+	classpath = class_path_or_name if classname != class_path_or_name else None
+	frame = utils.inspect_utils.caller_frame()
+	_globals = globals or frame.f_globals
+	if classname in _globals:
+		cls = _globals[classname]
+	elif classname in __builtins__:
+		cls = __builtins__[classname]
+	else:
+		return None
+	if classpath is not None:
+		found_class_path = class_path(cls)
+		if found_class_path != classpath:
+			return None
+	return cls
--- Python/utils/file.py
+++ Python/utils/file.py
@@ -1,13 +1,28 @@
 import os
 from datetime import datetime as _datetime
 
+import utils.function
+from utils.log.logger import *
 
-def backup_path(path, datetime=None, date_format="%Y-%m-%d--%H-%M-%S"):
+log = Logger()
+
+def backup_path(path, datetime=None, date_format=None):
+	_datetime = datetime or _datetime.now()
+	_date_format = date_format or "%Y-%m-%d_%H-%M-%S"
 	def gen_backup_path(cnt):
 		cnt_addition = f"_{cnt}" if cnt > 0 else ""
-		current_datetime = datetime or _datetime.now()
-		return path + "." + current_datetime.strftime(date_format) + cnt_addition + ".bak"
+		return path + "." + _datetime.strftime(_date_format) + cnt_addition + ".bak"
 	cnt = 0
 	while os.path.exists(backup_path := gen_backup_path(cnt)):
 		cnt += 1
 	return backup_path
+
+def backup(path, datetime=None, date_format=None):
+	target_path = backup_path(path, datetime, date_format)
+	log.warning(utils.function.msg(f"Backing up to '{target_path}'"))
+	try:
+		os.rename(path, target_path)
+	except Exception as e:
+		log.error(utils.function.msg(f"Error backing up file '{path}' to '{target_path}': {e}"))
+		return None
+	return target_path
