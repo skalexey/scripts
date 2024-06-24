b37bb7776d8055f6005649f86728205afa386756
skalexey <skorohodov-ad@narod.ru>
2024-06-24 19:59:49 +0200

    import_utils

--- Python/utils/import_utils.py
+++ Python/utils/import_utils.py
@@ -3,27 +3,14 @@ import importlib
 import utils.class_utils as class_utils
 
 
-def find_class(class_path): # e.g. 'module.submodule.ClassName'
-	module_name, class_name = class_path.rsplit('.', 1)
-	if class_name in globals():
-		cls = globals()[class_name]
-	elif class_name in __builtins__:
-		cls = __builtins__[class_name]
-	else:
-		return None
-	cls_path = class_utils.class_path(cls)
-	if cls_path == class_path:
-		return cls
-	return None
-
 def find_or_import_class(class_path): # e.g. 'module.submodule.ClassName'
 	# Try to find the class in globals()
-	cls = find_class(class_path)
+	cls = class_utils.find_class(class_path)
 	if cls:
 		return cls
 	# If not found in globals(), attempt to import the module and retrieve the class
+	module_name, class_name = class_path.rsplit('.', 1)
 	try:
-		module_name, class_name = class_path.rsplit('.', 1)
 		module = importlib.import_module(module_name)
 		return getattr(module, class_name)
 	except (ImportError, AttributeError, ValueError) as e:
