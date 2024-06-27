3089ef16d8c13470741402070968ddcd51be7dcf
skalexey <skorohodov-ad@narod.ru>
2024-06-27 19:19:09 +0200

    Archives

--- Python/archives/point2_archive.py
+++ Python/archives/point2_archive.py
@@ -57,3 +57,68 @@ class Point2(Point):
 	def __repr__(self):
 		return f"Point2({self.data})"
 	
+
+
+
+
+
+
+
+
+# V2
+
+from .point import *
+
+
+class Point2(Point):
+	# Arguments:
+	# 	*args: coordinates x, y | (x, y) | [x, y]
+	def __init__(self, *args, x=None, y=None, **kwargs):
+		super().__init__()
+		data = None 
+		if len(args) == 2:
+			# If two arguments are provided, assume they are x and y coordinates
+			if x is not None or y is not None:
+				raise ValueError(utils.function.msg("Multiple values for x, y provided"))
+			data = args
+		elif len(args) == 1:
+			# If only one argument is provided, assume it is a list or tuple containing x and y coordinates
+			if len(args[0]) != 2:
+				raise ValueError(utils.function.msg("Data must contain exactly two elements"))
+			data = list(args[0])
+		elif len(args) == 0:
+			# If no arguments provided, assume the point is at the origin
+			data = [0, 0]
+		else:
+			raise ValueError(utils.function.msg("Invalid number of arguments"))
+		if data is None:
+			raise ValueError(utils.function.msg("Unexpected error"))
+		self.data = data
+	
+	def _get_x(self):
+		return self.data[0]
+
+	def _set_x(self, value):
+		self.data[0] = value
+
+	def _get_y(self):
+		return self.data[1]
+
+	def _set_y(self, value):
+		self.data[1] = value
+
+	x = property(
+		fget=_get_x,
+		fset=_set_x,
+		doc="The x coordinate property."
+	)
+
+	y = property(
+		fget=_get_y,
+		fset=_set_y,
+		doc="The y coordinate property."
+	)
+
+	def __repr__(self):
+		return f"Point2({self.data})"
+	
