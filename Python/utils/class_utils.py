57fa68bae093c1e42176500763a322f2cd4d7e2c
skalexey <skorohodov-ad@narod.ru>
2024-06-27 19:30:53 +0200

    Load classes by full path.

--- Python/utils/class_utils.py
+++ Python/utils/class_utils.py
@@ -1,16 +1,20 @@
+import inspect
+
+import utils.function
 import utils.inspect_utils
 
 
 def class_path(cls_or_name):
-	if isinstance(cls_or_name, str):
+	if inspect.isclass(cls_or_name):
+		cls = cls_or_name
+	elif isinstance(cls_or_name, str):
 		frame = utils.inspect_utils.caller_frame()
 		cls = find_class(cls_or_name, globals=frame.f_globals)
 		if cls is None:
-			raise ValueError(f"Couldn't find class '{cls_or_name}' in globals() on the call level nor in __builtins__")
-		return cls
+			raise ValueError(utils.function.msg(f"Couldn't find class '{cls_or_name}' in globals() on the call level nor in __builtins__"))
 	else:
-		return f"{cls_or_name.__module__}.{cls_or_name.__name__}"
-	return f"{cls.__module__}.{cls.__name__}"
+		raise ValueError(utils.function.msg(f"Expected class or class name, got {cls_or_name}"))
+	return f"{cls.__module__}.{cls.__qualname__}"
 
 def class_name(cls_or_path):
 	if isinstance(cls_or_path, str):
--- Python/utils/import_utils.py
+++ Python/utils/import_utils.py
@@ -1,17 +1,126 @@
 import importlib
+import inspect
+import os
+import sys
+import threading
 
 import utils.class_utils as class_utils
+import utils.function
+import utils.log.logger
+from utils.profile.profiler import TimeProfiler
 
+profiler = TimeProfiler()
+
+log = utils.log.logger.Logger()
+
+_module_cache = None
+_cache_lock = threading.Lock()
+_cache_loading_thread = None
+
+
+def module_cache():
+	if _module_cache is None:
+		with _cache_lock:
+			global _cache_loading_thread
+			if _cache_loading_thread is not None:
+				return None
+			log.debug("Building module cache")
+			def work():
+				global _module_cache
+				profiler.start()
+				cache = {}
+				search_dirs = collect_search_paths()
+				for dir_path in search_dirs:
+					for root, dirs, files in os.walk(dir_path):
+						dirname = os.path.basename(root)
+						if dirname.startswith('.'):
+							continue
+						if dirname == '__pycache__':
+							continue
+						if '__init__.py' not in files:
+							continue
+						files.remove('__init__.py')
+						for file in files:
+							if file.endswith('.py'):
+								full_fpath = os.path.join(root, file)
+								relpath = os.path.relpath(full_fpath, dir_path)
+								module_path = os.path.splitext(relpath)[0].replace(os.path.sep, '.')
+								# log.debug(f"Found module: {module_path} in dir_path: '{dir_path}', root: '{root}', file: '{file}'")
+								assert module_path not in cache
+								cache[module_path] = full_fpath
+				profiler.print_measure()
+				log.debug(f"module_cache(): Found {len(cache)} modules")
+				_module_cache = cache
+			# Run task in parallel
+			_cache_loading_thread = threading.Thread(target=work)
+			_cache_loading_thread.start()
+			
+	return _module_cache
+
+def is_module_path(path):
+	cache = module_cache()
+	if cache is None: # Cache not ready yet
+		log.debug(utils.function.msg(f"Cache not ready yet. Working using find_spec()"))
+		try:
+			spec = importlib.util.find_spec(path)
+			return spec is not None
+		except ImportError:
+			return False
+	return path in cache
+
+def collect_search_paths():
+	# Start with the system paths
+	# Put real paths in a set to avoid duplicates
+	paths = set(os.path.realpath(path) for path in sys.path)
+	
+	# Add the current working directory
+	cwd =  os.path.realpath(os.getcwd())
+	paths.add(cwd)
+	
+	# Add site packages directories (if available)
+	try:
+		from distutils.sysconfig import get_python_lib
+		site_packages = os.path.realpath(get_python_lib())
+		paths.add(site_packages)
+	except ImportError:
+		pass  # Handle the case where distutils is not available
+
+	# # Add the directory of the script (if running as a script)
+	# script_dir = os.path.dirname(os.path.abspath(__file__))
+	# if script_dir not in paths:
+	# 	paths.add(script_dir)
+	
+	return paths
 
 def find_or_import_class(class_path): # e.g. 'module.submodule.ClassName'
 	# Try to find the class in globals()
 	cls = class_utils.find_class(class_path)
 	if cls:
 		return cls
+
 	# If not found in globals(), attempt to import the module and retrieve the class
-	module_name, class_name = class_path.rsplit('.', 1)
-	try:
-		module = importlib.import_module(module_name)
-		return getattr(module, class_name)
-	except (ImportError, AttributeError, ValueError) as e:
-		raise ValueError(f"Error finding or importing class '{class_name}': {str(e)}")
+	module_path, class_name = class_path.rsplit('.', 1)
+	i = 1
+	while True:
+		split_result = class_path.rsplit('.', i)
+		if len(split_result) < i:
+			break
+		module_path, class_name = split_result[0:2]
+		if module_path in sys.modules:
+			module = sys.modules[module_path]
+		else:
+			if not is_module_path(module_path):
+				i += 1
+				continue
+			module = importlib.import_module(module_path)
+		where = module
+		for i in range(1, len(split_result)):
+			class_name = split_result[i]
+			cls = getattr(where, class_name, None)
+			if not inspect.isclass(cls):
+				raise ValueError(utils.function.msg(f"Expected class, got {cls}"))
+			if cls is None:
+				return None
+			where = cls
+		return cls
+	raise ValueError(utils.function.msg(f"Couldn't find class '{class_path}' nor import it"))
