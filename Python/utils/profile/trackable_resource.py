d0b587d6b0380fdf4f4f34d7b132785f1c1407a6
skalexey <skorohodov-ad@narod.ru>
2024-07-04 13:03:45 +0200

    TrackableResource

--- Python/utils/profile/trackable_resource.py
+++ Python/utils/profile/trackable_resource.py
@@ -1,19 +1,20 @@
-import utils.lang
-from utils.log.logger import *
+import weakref
+
+import utils.method
+from utils.log.logger import Logger
 
 log = Logger()
 
 class TrackableResource:
 	def __repr__(self):
 		super_repr = super().__repr__()
-		return f"{self.__class__.__name__}({super_repr})"
+		_memid = id(self)
+		return f"{self.__class__.__name__}(memid={_memid}{super_repr})"
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		log.verbose(f"TrackableResource: {self}.__init__()")
-
-	def __del__(self):
-		object_info = super(object, self).__str__()
-		log.verbose(f"TrackableResource: {object_info}.__del__()")
-		# Clear all resources stored in the object's attributes since overriding __del__ method changes the behavior of garbage collection
-		utils.lang.clear_resources(self)
+		log.verbose(utils.function.msg_v())
+		identifier = f"TrackableResource: {self!r}"
+		def on_destroyed():
+			log.verbose(utils.function.msg(identifier))
+		self._finalizer = weakref.finalize(self, on_destroyed)
