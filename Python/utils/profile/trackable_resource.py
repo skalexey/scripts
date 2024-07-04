d28990bc913432d68f1721195e31a7911bb2c934
skalexey <skorohodov-ad@narod.ru>
2024-07-04 19:54:10 +0200

    TrackableResource

--- Python/utils/profile/trackable_resource.py
+++ Python/utils/profile/trackable_resource.py
@@ -1,11 +1,26 @@
 import weakref
 
 import utils.method
+from utils.collection.associative_list import AssociativeList
 from utils.log.logger import Logger
 
 log = Logger()
 
+class TrackableResourceInfo:
+	def __init__(self):
+		self.id = None
+		self.ref = None
+		self._repr = None
+
+	@property
+	def repr(self):
+		obj = self.ref()
+		if obj is not None:
+			self._repr = f"TrackableResource: {obj!r}" # Always store the latest repr
+		return self._repr
+
 class TrackableResource:
+	resources = AssociativeList()
 	def __repr__(self):
 		super_repr = super().__repr__()
 		_memid = id(self)
@@ -13,8 +28,10 @@ class TrackableResource:
 
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+		info = TrackableResourceInfo()
+		def on_destroyed(ref):
+			log.verbose(utils.function.msg(info.repr))
+			TrackableResource.resources.remove(info.id)
+		info.ref = weakref.ref(self, on_destroyed)
+		info.id = self.resources.add(info)
 		log.verbose(utils.function.msg_v())
-		identifier = f"TrackableResource: {self!r}"
-		def on_destroyed():
-			log.verbose(utils.function.msg(identifier))
-		self._finalizer = weakref.finalize(self, on_destroyed)
