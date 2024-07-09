0b2d990b8036d305eb1347b6550887521b399269
skalexey <skorohodov-ad@narod.ru>
2024-07-09 11:10:48 +0200

    Tests

--- Python/tests/inheritance_test.py
+++ Python/tests/inheritance_test.py
@@ -1,4 +1,11 @@
+from abc import ABC, ABCMeta, abstractmethod
 from test import *
+
+import utils.class_utils
+import utils.class_utils as class_utils
+import utils.function
+import utils.method
+from PySide6.QtWidgets import QApplication, QWidget
 
 
 def init_test():
@@ -38,10 +45,65 @@ def init_test():
 	log.expr("f = F()")
 	log(title("End of Init Test"))
 	
+def abc_test():
+	log(title("ABC Test"))
+	app = QApplication([])
+	class A(ABC):
+		@abstractmethod
+		def a(self):
+			pass
+
+	class B:
+		pass
+
+	class AbstractMethodNotDefined(A, B):
+		pass
+
+	class D(QWidget):
+		pass
+
+	try:
+		class MetaclassConflictQt(A, D):
+			pass
+	except TypeError as e:
+		log(f"Expected exception: {e}")
+
+	assert_exception("amnd = AbstractMethodNotDefined()")
+
+	class CustomMeta(type):
+		pass
+
+	try:
+		class MetaclassConflict(ABC, metaclass=CustomMeta):
+			pass
+	except TypeError as e:
+		log(f"Expected exception: {e}")
+
+	class CombinedMeta(class_utils.EnforcedABCMeta, type(QWidget)):
+		pass
+
+	class F(ABC, metaclass=CombinedMeta):
+		@abstractmethod
+		def f(self):
+			pass
+
+	class G(F, QWidget):
+		def a(self):
+			log(utils.method.msg_kw())
+
+	# Test to check if the abstract method enforcement works
+	try:
+		g = G()  # This should raise a TypeError
+		assert False, "This should not be reached"
+	except TypeError as e:
+		log(f"Expected TypeError caught: {e}")
+
+	log(title("End of ABC Test"))
 
 def test():
 	log(title("Inheritance Test"))
-	init_test()
+	# init_test()
+	abc_test()
 	log(title("End of Inheritance Test"))
 
 run()
