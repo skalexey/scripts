954624815d5ae650d1eff1c4cd5c8649fa435432
skalexey <skorohodov-ad@narod.ru>
2024-07-15 10:07:50 +0200

    Code cleaning

--- Python/utils/memory.py
+++ Python/utils/memory.py
@@ -15,29 +15,32 @@ log = Logger()
 class WeakCallable:
 	def __init__(self, cb, owner=None, *args, **kwargs):
 		def on_destroyed(ref):
-			log.warning(f"Callable has been garbage collected")
+			log.verbose(f"Callable has been garbage collected")
 		if owner is not None:
-			if not hasattr(owner, "__weakcbs__"):
-				owner.__weakcbs__ = []
-			owner.__weakcbs__.append(cb)
+			weakcbs = getattr(owner, "__weakcbs__", None)
+			if weakcbs is None:
+				weakcbs = []
+				owner.__weakcbs__ = weskcbs
+			weakcbs.append(cb)
 			owner_ref = weakref.ref(owner)
 			def remove_from_owner():
 				owner = owner_ref()
 				if owner is not None:
-					owner.__weakcbs__.remove(self)
-					if len(owner.__weakcbs__) == 0:
-						del owner.__weakcbs__
+					weakcbs.remove(self)
+					if len(weakcbs) == 0:
+						del weakcbs
 			on_destroyed_base = on_destroyed
 			def on_destroyed(ref):
 				on_destroyed_base(ref)
 				owner = owner_ref()
 				if owner is not None:
-					log.debug(f"Removing weak callable from the owner '{owner}'")
-					owner.__weakcbs__.remove(self)
-					if len(owner.__weakcbs__) == 0:
+					log.verbose(f"Removing weak callable from the owner '{owner}'")
+					weakcbs = owner.__weakcbs__
+					weakcbs.remove(self)
+					if len(weakcbs) == 0:
 						del owner.__weakcbs__
 				else:
-					log.debug(f"Owner has been garbage collected")
+					log.verbose(f"Owner has been garbage collected")
 		self._ref = weakref.ref(cb, on_destroyed)
 		super().__init__(*args, **kwargs)
 
