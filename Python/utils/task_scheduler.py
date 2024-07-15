5ae1d7b9363ca422d1d452bb9dce7e32b78109f2
skalexey <skorohodov-ad@narod.ru>
2024-07-15 10:18:55 +0200

    TaskScheduler: task cancellation

--- Python/utils/task_scheduler.py
+++ Python/utils/task_scheduler.py
@@ -1,12 +1,14 @@
 # TODO: Wait for the task utilization after cancelling through a future
 import asyncio
+import concurrent.futures
 import threading
 import time
 import weakref
 from collections import deque
 
-import utils.asyncio_utils
+import utils.asyncio_utils as asyncio_utils
 import utils.method
+from utils.live import verify
 from utils.log.logger import Logger
 from utils.memory import SmartCallable
 from utils.profile.trackable_resource import TrackableResource
@@ -39,15 +41,19 @@ class TaskScheduler(TrackableResource):
 	def loop(self):
 		with self._lock:
 			if self._loop is None:
-				self._loop = utils.asyncio_utils.get_event_loop()
+				self._loop = asyncio_utils.get_event_loop()
 				self._thread_id = threading.current_thread().ident
 		return self._loop
+
+	def is_current_thread_loop_owner(self):
+		thread_id = threading.current_thread().ident
+		return thread_id == self._thread_id
 
 	class TaskInfo:
 		def __init__(self, function, task = None, future=None):
 			self.function = function
 			self.task = task
-			self.future = future or utils.asyncio_utils.get_event_loop().create_future()
+			self.future = future or asyncio_utils.get_event_loop().create_future()
 			super().__init__()
 			# self.future = future or asyncio.Future()
 
@@ -68,13 +74,43 @@ class TaskScheduler(TrackableResource):
 		raise "Not implemented yet."
 
 	def wait_all_tasks(self):
+		if len(self._tasks) == 0:
+			log.debug(utils.method.msg_kw("Nothing to wait for"))
+			return
 		tasks = []
 		for task_info in list(self._tasks.values()):
 			tasks.append(task_info.task)
+		log.debug(utils.method.msg_kw(f"Waiting for {len(tasks)} tasks to finish: {tasks}"))
 		async def wait_tasks():
 			# Suppresses CancelledError exceptions here, but they will still be originally raised in the coros
 			await asyncio.gather(*tasks, return_exceptions=True)
-		self.loop.run_until_complete(wait_tasks())
+		try:
+			if self.is_current_thread_loop_owner():
+				if self.loop.is_running():
+					raise RuntimeError("Called wait_all_tasks() from a running task.")
+					# log.warning(utils.method.msg_kw("Called wait_all_tasks() from a running task. It will be ignored"))
+					# return
+				result = self.loop.run_until_complete(wait_tasks())
+			else:
+				log.debug(utils.method.msg_kw("Called wait_all_tasks() from a different thread. Waiting for the tasks to finish in the same loop using asyncio.run_coroutine_threadsafe"))
+				futures = []
+				for task_info in list(self._tasks.values()):
+					futures.append(task_info.future)
+				async def wait_tasks():
+					last_exception = None
+					for future in futures:
+						try: # Allow all the tasks to complete
+							await future
+						except BaseException as e:
+							log.error(utils.method.msg_kw(f"Exception occurred while waiting for a task future '{future}': {e}"))
+							last_exception = e
+				future = asyncio.run_coroutine_threadsafe(wait_tasks(), self.loop)
+				result = future.result() # Wait and raise exception if occurred in the end. It will never complete the tasks, but just wait for the owner thread to complete them.
+				log.debug(utils.method.msg_kw(f"other thread waiting result: {result}"))
+		except BaseException as e:
+			log.error(utils.method.msg_kw(f"Exception occurred while waiting for the tasks: {e}"))
+			raise
+		return result
 
 	def wait(self, future):
 		future.get_loop().run_until_complete(future)
@@ -87,15 +123,27 @@ class TaskScheduler(TrackableResource):
 
 	# Use this method in a loop if your application doesn't have an event loop running
 	def update(self, dt):
+		log.debug(utils.method.msg_kw())
 		# Create the loop from the updating thread if it doesn't exist
 		self.loop
 		# Update the tasks
 		if len(self._tasks) > 0:
-			future = next(iter(self._tasks.values())).future
+			task_info = next(iter(self._tasks.values()))
+			future, task = task_info.future, task_info.task
 			async def update_task():
 				await asyncio.sleep(self.update_interval)
 			cur = time.time()
+			cancelled = _check_future_task_cancelled(future, task)
+			# verify(not task.cancelled(), "Task is cancelled but it still exists in the tasks list") # The task must be removed from the tasks list in the _on_task_done method
+			if task.cancelled():
+				log.warning("Task is cancelled but it still exists in the tasks list") # The task must be removed from the tasks list in the _on_task_done method
 			future.get_loop().run_until_complete(update_task())
+			if future.done():
+				if not future.cancelled():
+					ex = future.exception()
+					if ex is not None:
+						log.error(utils.method.msg_kw(f"Exception occurred while updating the tasks: {ex}"))
+						raise ex
 			taken_time = time.time() - cur
 			self.on_update.notify(taken_time)
 			TaskScheduler.on_update.notify(self, taken_time)
@@ -124,7 +172,7 @@ class TaskScheduler(TrackableResource):
 			else:
 				return task_info.future
 		# Don't fit by queue size
-		log.debug(utils.method.msg_kw(f"Task has been cancelled due to the queue size limit"))
+		log.debug(utils.method.msg_kw(f"Task scheduling has been ignored due to the queue size limit"))
 		return None
 
 	def _create_task_info(self, async_function):
@@ -133,8 +181,7 @@ class TaskScheduler(TrackableResource):
 		return task_info
 
 	def _run_next(self):
-		thread_id = threading.current_thread().ident
-		# assert(thread_id == self._thread_id)
+		# assert(self.is_current_thread_loop_owner())
 		with self._lock:
 			task_info = self._queue.popleft()
 			assert task_info.task is None
@@ -145,6 +192,7 @@ class TaskScheduler(TrackableResource):
 				task = self.loop.create_task(task_info.function())
 				task_info.task = task
 				self._tasks[task] = task_info
+				log.debug(utils.method.msg(f"Task {task} has been registered. Total task count: {len(self._tasks)}"))
 				assert len(self._tasks) == 1
 				task.add_done_callback(self._on_task_done)
 
@@ -153,30 +201,44 @@ class TaskScheduler(TrackableResource):
 				future = task_info.future
 
 				def future_done(future):
-					if future.cancelled():
-						log.debug(utils.method.msg(f"Future {future} has been cancelled"))
-						task.cancel()
-
+					_check_future_task_cancelled(future, task)
+				
 				future.add_done_callback(future_done)
 				return future
 
 			# Schedule the task creation in a thread-safe manner
-			loop = self.loop
+			# loop = self.loop
 			# future = asyncio.run_coroutine_threadsafe(create_task(), loop).result()
 			future = create_task()
 			return future
 
 	def _on_task_done(self, task):
+		log.debug(utils.method.msg_kw())
 		task_info = self._tasks.pop(task)
 		assert task_info == self._current_task_info
 		task, future = task_info.task, task_info.future
-		if task.cancelled():
-			log.debug(utils.method.msg(f"Task has been cancelled (task_info: {task_info})"))
+		try:
+			if not _check_future_task_cancelled(future, task):
+				if not future.done():
+					future.set_result(task.result())
+		except Exception as e:
+			log.error(f"Exception in _on_task_done: {e}")
 			if not future.done():
-				future.cancel()
-		else:
-			if not future.done():
-				future.set_result(task.result())
-		self._current_task_info = None
-		if len(self._queue) > 0:
-			self._run_next()
+				future.set_exception(e)
+		finally:
+			self._current_task_info = None
+			if len(self._queue) > 0:
+				self._run_next()
+
+def _check_future_task_cancelled(future, task):
+	if future.cancelled():
+		log.debug(utils.method.msg_kw(f"Future has been cancelled"))
+		if not task.cancelled():
+			task.cancel()
+	elif task.cancelled():
+		log.debug(utils.method.msg_kw(f"Task has been cancelled"))
+		if not future.done():
+			future.cancel()
+	else:
+		return False
+	return True
