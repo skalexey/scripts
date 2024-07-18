a6a1a750f79fcfd09f0013764929b4cbeadc46db
skalexey <skorohodov-ad@narod.ru>
2024-07-18 17:13:32 +0200

    utils.Job

--- Python/utils/job.py
+++ Python/utils/job.py
@@ -7,19 +7,27 @@ class Job(TrackableResource):
 	def __init__(self, *args, **kwargs):
 		self.on_done = Subscription()
 		self.on_cancel = Subscription()
+		self._cancelled = False
 		super().__init__(*args, **kwargs)
 
 	def update(self, dt):
 		if self.is_done():
-			self.on_done.notify()
+			if self.is_cancelled():
+				self.on_cancel.notify(self)
+			else:
+				self.on_done.notify(self)
 			return True
 		return False
 
 	def is_done(self):
-		return False
+		return self.is_cancelled() # Default
+
+	def is_cancelled(self):
+		return self._cancelled
 
 	def cancel(self):
-		self.on_cancel.notify()
+		self._cancelled = True
+		self.on_cancel.notify(self)
 
 class TimedJob(Job):
 	def __init__(self, duration = None, duration_range=None, *args, **kwargs):
@@ -32,6 +40,8 @@ class TimedJob(Job):
 		super().__init__(*args, **kwargs)
 
 	def is_done(self):
+		if super().is_done():
+			return True
 		return self.time_elapsed >= self.duration
 
 	def update(self, dt):
