66905f62699dd693af51c45170282ea214624cc7
skalexey <skorohodov-ad@narod.ru>
2024-07-18 15:11:57 +0200

    Archives

--- Python/utils/archive/task_scheduler_archive.py
+++ Python/utils/archive/task_scheduler_archive.py
@@ -50,3 +50,59 @@ def wait_all_tasks(self):
 		except asyncio.CancelledError:
 			log.info(f"Task has been cancelled (tas_info: {task_info})")
 
+
+
+# V1
+class LoopOperatorEnter:
+	def __init__(self, loop=None, try_use=None, check_if_free=None, operator=None):
+		self.loop = loop
+		self.try_use = try_use
+		self.check_if_free= check_if_free
+		self._try_result = None
+		self._check_result = None
+		self.operator = operator
+
+	def try_result(self):
+		return self._try_result
+
+	def __getattr__(self, name):
+		return getattr(self.operator, name)
+
+	def __enter__(self):
+		with self.operator.enter_lock:
+			log.debug(utils.method.msg_kw(f"Thread '{threading.current_thread().name}' is entering the loop operator"))
+			if self.operator.is_operating():
+				msg = utils.method.msg_kw(f"Loop is already operating by '{self.operator.thread_id}'")
+				if self.try_use:
+					log.debug(msg)
+					self._try_result = False
+					return self
+				raise RuntimeError(msg)
+			if self.loop.is_running():
+				raise RuntimeError("Unknown operator is already running the loop")
+			if self.try_use:
+				self._try_result = True
+			self.operator.thread_id = threading.current_thread().name
+			log.debug(utils.method.msg_kw(f"Thread '{self.operator.thread_id}' entered the loop operator"))
+			return self
+	
+	def __exit__(self, exc_type, exc_value, traceback):
+		operator_thread_id = self.operator.thread_id
+		is_owner = operator_thread_id == threading.current_thread().name
+		msg_addition = "" if is_owner else " (not the owner)"
+		log.debug(utils.method.msg_kw(f"Thread '{operator_thread_id}'{msg_addition} is exiting the loop operator"))
+		if is_owner:
+			self.operator.thread_id = None
+		log.debug(utils.method.msg_kw(f"Thread '{operator_thread_id}'{msg_addition} exited the loop operator"))
+
+def __call__(self, loop):
+	params = self.LoopOperatorEnter(loop=loop, operator=self)
+	return params
+
+def try_use(self, loop):
+	params = self.LoopOperatorEnter(loop=loop, try_use=True, operator=self)
+	return params
+
+def check_if_free(self, loop):
+	params = self.LoopOperatorEnter(loop=loop, check_if_free=True, operator=self)
+	return params
