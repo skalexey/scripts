71c251fa6578334bebdd3e8ad8a30991cc414d02
skalexey <skorohodov-ad@narod.ru>
2024-08-08 22:00:58 +0200

    timed_loop

--- Python/utils/timed_loop.py
+++ Python/utils/timed_loop.py
@@ -5,14 +5,18 @@ from utils.debug.debug_detector import debug_timespan
 
 
 class Controller:
-	def __init__(self):
+	def __init__(self, timeout):
+		self.timeout = timeout
 		self.last_time = time()
 		self.elapsed_time = 0
 		self.attempt = 0
 		self.timedout = False
 
-	def iterate(self, timeout):
+	def iterate(self):
 		self.attempt += 1
+		return self.update()
+
+	def update(self):
 		current_time = time()
 		dt = current_time - self.last_time
 		self.elapsed_time += dt
@@ -20,7 +24,7 @@ class Controller:
 			_debug_timespan = debug_timespan(self)
 			self.elapsed_time -= min(self.elapsed_time, _debug_timespan)
 		assert self.elapsed_time >= 0, f"Elapsed time is negative: {self.elapsed_time}"
-		if self.elapsed_time >= timeout:
+		if self.elapsed_time >= self.timeout:
 			self.timedout = True
 		self.last_time = current_time
 		return not self.timedout
@@ -29,16 +33,16 @@ class Controller:
 		return f"{self.__class__.__name__}(attempt={self.attempt}, elapsed_time={self.elapsed_time}, timedout={self.timedout})"
 
 def timed_loop(timeout):
-	controller = Controller()
-	while controller.iterate(timeout):
+	controller = Controller(timeout)
+	while controller.iterate():
 		yield controller  # Yield control back to the user’s block
 	yield controller
 
 def timedout(timeout):
-		controller = Controller()
-		while controller.iterate(timeout):
-			yield controller.timedout  # Yield control back to the user’s block
-		yield controller.timedout
+	controller = Controller()
+	while controller.iterate(timeout):
+		yield controller.timedout  # Yield control back to the user’s block
+	yield controller.timedout
 
 
 if __name__ == "__main__":
