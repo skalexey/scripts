ebd5e07ba4aa614ff0b90fff8bca4e663b0ec5a6
skalexey <skorohodov-ad@narod.ru>
2024-08-08 21:47:51 +0200

    Atomic

--- Python/utils/concurrency/atomic.py
+++ Python/utils/concurrency/atomic.py
@@ -15,13 +15,27 @@ class Atomic:
 		self.lock.release()
 
 	def __getattr__(self, name):
-		with self.lock:
-			return getattr(self.obj, name)
-
+		attr = getattr(self.obj, name)
+		if callable(attr):
+			# Wrap the method to acquire the lock
+			def wrapped(*args, **kwargs):
+				with self.lock:
+					return attr(*args, **kwargs)
+			return wrapped
+		return attr
+		
 	def __setattr__(self, name, value):
 		with self.lock:
 			setattr(self.obj, name, value)
 
+	# Non-blocking reads
+	def __len__(self):
+		return len(self.obj)
+
+	def __getitem__(self, key):
+		return self.obj[key]
+
+	# Additional interface
 	def update(self, new_obj):
 		with self.lock:
 			self.obj = new_obj
