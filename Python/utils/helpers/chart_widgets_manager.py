1f862a4af412b35f53e27ee57184b8ca24635442
skalexey <skorohodov-ad@narod.ru>
2024-08-21 19:47:33 +0200

    ChartWidgetsManager

--- Python/utils/helpers/chart_widgets_manager.py
+++ Python/utils/helpers/chart_widgets_manager.py
@@ -1,21 +1,35 @@
+import weakref
 from abc import ABC, abstractmethod
 
 from utils.helpers.data_widgets_update import DataWidgetsUpdateMixin
 
 
 class ChartWidgetsManager(DataWidgetsUpdateMixin, ABC):
-	def __init__(self, app_context, *args, **kwargs):
+	def __init__(self, app_context, *args, parent=None, **kwargs):
+		self.set_parent_widget(parent)
 		super().__init__(*args, **kwargs)
 		self.app_context = app_context
 
 	# DataWidgetsUpdateMixin overrides
-	def _add_widget(self, data):
-		widget = super()._add_widget(data)
+	def _add_widget(self, data, *args, **kwargs):
+		widget = super()._add_widget(data, *args, **kwargs)
 		if widget is not None:
-			chart = self.app_context.main_window.chart_view
-			chart.scene.addItem(widget)
+			parent = self.parent_widget
+			if parent is not None:
+				widget.setParentItem(parent)
+			else:
+				chart = self.app_context.main_window.chart_view
+				chart.scene.addItem(widget)
 		return widget
 
 	def _destroy_widget(self, widget):
-		chart = self.app_context.main_window.chart_view
-		chart.scene.removeItem(widget)
+		scene = widget.scene()
+		scene.removeItem(widget)
+		widget.setParentItem(None)
+
+	@property
+	def parent_widget(self):
+		return self._parent_widget_ref() if self._parent_widget_ref is not None else None
+
+	def set_parent_widget(self, parent_widget):
+		self._parent_widget_ref = weakref.ref(parent_widget) if parent_widget is not None else None
