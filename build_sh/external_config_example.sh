29844cc4e56b80776c3ca704e8f327d39fdd38ee
Alex <biggun@writeme.com>
2024-12-26 21:02:51 -0600

    external_config_example.sh update

--- build_sh/external_config_example.sh
+++ build_sh/external_config_example.sh
@@ -4,13 +4,16 @@ function job()
 	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 	source $THIS_DIR/os.sh
 
-	if is_windows; then
-		export {TPL_NAME}_deps="${HOME}/Projects"
+	if [ ! -z $build_deps ]; then
+		export {TPL_NAME}_deps=$build_deps
 	else
-		export {TPL_NAME}_deps="${HOME}/Projects"
+		if is_windows; then
+			export {TPL_NAME}_deps="${HOME}/Projects"
+		else
+			export {TPL_NAME}_deps="${HOME}/Projects"
+		fi
+		export build_deps=${TPL_NAME}_deps
 	fi
-
-	[ ! -z {TPL_NAME}_deps ] && build_deps=${TPL_NAME}_deps
 }
 
 job $@
