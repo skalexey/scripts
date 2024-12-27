717c6804de3ede1649044586355d54878842c614
Alex <biggun@writeme.com>
2024-12-27 11:38:56 -0600

    code cleaning

--- automation/include/print_args_job.sh
+++ automation/include/print_args_job.sh
@@ -2,15 +2,15 @@
 
 # This is a handy job which can be used as a dummy for just checking the work of
 # the parent job printing its output arguments
-print_args_job()
+function print_args_job()
 {
 	source log.sh
 	local log_prefix="\033[0;33m[print_args_job]\033[0m: "
-	tmp=$@ # prevent from splitting the argument for the log function as list
-	[ -z "$1" ] && exit || log "\033[0;32m'$tmp'\033[0m"
+	local args=$@ # prevent from splitting the argument for the log function as list
+	[ -z "$1" ] && exit || log "\033[0;32m'$args'\033[0m"
 }
 
-job()
+function job()
 {
 	print_args_job $@
 }
