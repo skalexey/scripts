22a6085c0c23f1e658ef4527df8f81c9ee3acd3a
Alex <biggun@writeme.com>
2025-01-01 08:52:42 -0600

    invoke_function.sh

--- invoke_function.sh
+++ invoke_function.sh
@@ -7,7 +7,7 @@ function job()
 	[ -z "$1" ] && echo "No script provided. $usage" && exit 1 || local script="$1"
 	[ -z "$2" ] && echo "No command provided. $usage" && exit 2 || local command="$2"
 	source $script
-	${@:2}
+	"${@:2}"
 }
 
-job $@
+job "$@"
