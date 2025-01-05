dbc0349bb5ad77d2d053717108310d49dfbfe75a
Alex <biggun@writeme.com>
2025-01-05 10:58:13 -0600

    subscriptions.py

--- Python/utils/subscriptions.py
+++ Python/utils/subscriptions.py
@@ -1,5 +1,9 @@
+"""
+Global subscriptions for different purposes.
+"""
+
 from utils.log import log
-from utils.subscription import Event, Subscription
+from utils.subscription import Event
 
 on_exit = Event()
 on_exit.subscribe(lambda: log("Exiting..."), on_exit) # Called manually or by the main thread monitor
