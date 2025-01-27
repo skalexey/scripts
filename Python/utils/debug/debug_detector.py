b17714beb8e328e04824c05ba52f2a1802e50e6a
Alex <biggun@writeme.com>
2025-01-27 07:41:37 -0600

    DebugDetector performance

--- Python/utils/debug/debug_detector.py
+++ Python/utils/debug/debug_detector.py
@@ -22,21 +22,21 @@ class DebugDetector:
 		result = self._on_debug_detected_subscription
 		if result is None:
 			result = utils.subscription.Subscription()
+			result.subscribe(self._on_debug_detected)
 			self._on_debug_detected_subscription = result
-			self.on_debug_detected.subscribe(self._on_debug_detected)
 		return result
 
 	def _on_debug_detected(self):
 		self._last_debug_timespan_checkin.clear()
 
 	def check_debug_detection(self):
-		with self.debug_detection_lock:
-			current_time = datetime.now().timestamp()
-			dt = current_time - (self._last_check_time or current_time)
-			self._last_check_time = current_time
-			if dt > self.debug_detection_threshold:
-				self.last_debug_timespan = dt
-				self.on_debug_detected.notify()
+		# with self.debug_detection_lock:  # Does not worth it
+		current_time = datetime.now().timestamp()
+		dt = current_time - (self._last_check_time or current_time)
+		self._last_check_time = current_time
+		if dt > self.debug_detection_threshold:
+			self.last_debug_timespan = dt
+			self.on_debug_detected.notify()
 
 	def grab_last_debug_timespan(self, checker):
 		self.check_debug_detection()
