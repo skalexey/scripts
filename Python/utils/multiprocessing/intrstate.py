64bd90748bf8212473e46632a510580bd02311d4
Alex <biggun@writeme.com>
2025-02-09 20:23:29 -0600

    multiprocessing.intrstate

--- Python/utils/multiprocessing/intrstate.py
+++ Python/utils/multiprocessing/intrstate.py
@@ -9,7 +9,10 @@ class MultiprocessIntrstate(Intrstate):
 	This approach is useful for representing a data block as an object while concealing control information from the user.
 	"""
 
-	def __init__(self, *args, **kwargs):
-		if self._state is None:
-			self._state = multiprocessing.Manager().dict()
+	def __init__(self, state=None, *args, **kwargs):
+		if state is not None:
+			self._state = state
+		else:
+			if self._state is None:
+				self._state = multiprocessing.Manager().dict()
 		super().__init__(*args, **kwargs)
