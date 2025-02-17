9afb219fc73c852a370a19d0c799215d5277f110
Alex <biggun@writeme.com>
2025-02-17 12:57:53 -0600

    triangle_vertices_point

--- Python/utils/math/library.py
+++ Python/utils/math/library.py
@@ -4,3 +4,7 @@ def clamp(value, _min, _max):
 # direction 1 for up, -1 for down
 def triangle_vertices(origin, size, direction, cls):
 	return [cls(origin.x, origin.y - size * direction), cls(origin.x - size, origin.y + size * direction), cls(origin.x + size, origin.y + size * direction)]
+
+def triangle_vertices_point(origin, size, direction, cls):
+	double_size_direction = size * 2 * direction
+	return [cls(origin.x, origin.y), cls(origin.x - size, origin.y + double_size_direction), cls(origin.x + size, origin.y + double_size_direction)]
